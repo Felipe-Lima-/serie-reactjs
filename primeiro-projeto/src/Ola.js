1d39e70dcd760ad7aadda5749b2bd71b094eb662
Felipe Lima <philipe.templo@gmail.com>
2018-06-21 22:53:46 -0300

    Initial commit

---
+++ primeiro-projeto/src/Ola.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+
+class Ola extends Component{
+    render()
+    {
+        return(
+        <span>{this.props.name} </span>
+        )
+    }
+}
+export default Ola
